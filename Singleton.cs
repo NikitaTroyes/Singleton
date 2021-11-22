644538375132f939a9d1a13264de924e40c6885e
NT <89306099+NikitaTroyes@users.noreply.github.com>
2021-11-22 15:39:50 +0100

    Add files via upload

---
+++ Singleton.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton
+{
+    public sealed class DatabaseSingleton
+    {
+        private static Singleton instance = null;
+        private static readonly object padlock = new object();
+
+        DatabaseSingleton()
+        {
+        }
+
+        public static DatabaseSingleton Instance
+        {
+            get
+            {
+                lock (padlock)
+                {
+                    if (instance == null)
+                    {
+                        instance = new DatabaseSingleton();
+                    }
+                    return instance;
+                }
+            }
+        }
+    }
+}
